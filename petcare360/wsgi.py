47d69f91ef7eb78a750f9235ff0044fe70c09329
Vinoth Kumar <149368774+vinothkumar231@users.noreply.github.com>
2026-06-30 15:37:05 +0530

    Define 'app' for Vercel compatibility
    Add 'app' variable for Vercel's Python runtime.

--- petcare360/wsgi.py
+++ petcare360/wsgi.py
@@ -1,16 +1,13 @@
 """
 WSGI config for petcare360 project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "petcare360.settings")
 
 application = get_wsgi_application()
+app = application  # Vercel's @vercel/python runtime looks for "app"
