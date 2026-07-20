4140f802fea6209ff30f8dfc277c5d96ce36eb4b
Vinoth Kumar D <IZ1229@intelizest.com>
2026-07-20 14:27:18 +0530

    Fix MEDIA_ROOT for Vercel deployment to write to /tmp/media

--- petcare360/settings.py
+++ petcare360/settings.py
@@ -161,7 +161,10 @@ STORAGES = {
 }
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / "media"
+if os.environ.get("VERCEL"):
+    MEDIA_ROOT = "/tmp/media"
+else:
+    MEDIA_ROOT = BASE_DIR / "media"
 
 TIME_ZONE = 'Asia/Kolkata'
 USE_TZ = True
